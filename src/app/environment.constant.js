4cf5384aaa9da5dd51a85debbee37dce2fa044f7
gaokj <543144810@qq.com>
2016-10-14 10:28:30 +0800

    全部清空

--- src/app/environment.constant.js
+++
@@ -1,3 +0,0 @@
-angular.module("cooAdmin").constant("environment",{
-	host:"http://10.115.19.223:8091",
-})
--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('cooAdmin', ['ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
