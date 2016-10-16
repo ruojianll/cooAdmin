319d07a75f2a76341ff395cb9aca7c534a4e73f5
gaokj <543144810@qq.com>
2016-10-16 18:42:38 +0800

    10/16 user 创建 删除 修改

--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -1,18 +1,19 @@
 angular.module("cooAdmin").controller("login",function($scope,apiServ,environment,accountServ,$state){
-	  var _selected;
-	  $scope.selected = undefined;
-	  $scope.states = [];
-	  //获取所有用户
+	   	//login_in 检索功能
+	   	var _selected;
+	    $scope.selected = undefined;
+	    $scope.states = [];
+	  
 		apiServ.post("/api/manager/user/all",{}).then(
-				function(data){
-						for(var x in data){
-							$scope.states.push(data[x].user_name)
-						}
-						console.log($scope.states)
-				},
-				function(err){
-						console.log("cuowu")
-				}
+			function(data){
+					for(var x in data){
+						$scope.states.push(data[x].user_name)
+					}
+					console.log($scope.states)
+			},
+			function(err){
+					console.log("cuowu")
+			}
 		)
 		$scope.click = function(){
 			accountServ.login($scope.user_name,$scope.password).then(
@@ -25,35 +26,93 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 				}
 			)
 		}
-}).controller("homeCtrl",function($scope,apiServ,$state){	
-		if(localStorage.user_id && localStorage.web_token){
-//				return;
-		}else{
-				$state.go("login")
+}).controller("homeCtrl",function($scope,apiServ,$state){
+	$scope.vm={}
+	
+	if(localStorage.user_id && localStorage.web_token){
+
+	}else{
+		$state.go("login")
+	}
+	//获取所有用户
+	apiServ.post("/api/manager/user/all",{}).then(
+		function(data){
+//						console.log(data)
+			$scope.aUser = data;
+			$scope.arr_user = $scope.aUser.slice(0,5)
+			$scope.userItems = Math.ceil($scope.aUser.length / 5)*10;
+		},
+		function(err){
+				console.log("cuowu")
 		}
-		//获取所有用户
-		apiServ.post("/api/manager/user/all",{}).then(
-				function(data){
-						console.log(data)
-						$scope.aUser = data;
-						$scope.userItems = Math.ceil($scope.aUser.length / 5)*10;
-				},
-				function(err){
-						console.log("cuowu")
-				}
+	)
+	//user分页功能
+	$scope.userPage = function(){
+		$scope.arr_user = $scope.aUser.slice((this.currentPage-1)*5,this.currentPage*5)
+	}
+	//focus焦点
+	$scope.focus = function(){
+		$scope.vm.focus_name = this.x.user_name;
+		$scope.vm.focus_id = this.x.id;
+		console.log(this)
+	}
+	//编辑
+	$scope.edit = function(){
+//		console.log($scope.vm.focus_id)
+//		console.log($scope.vm.focus_name)
+//		console.log($scope.vm.focus_password)
+		apiServ.post("/api/manager/user/edit",{
+			user_id:$scope.vm.focus_id,
+		    user_name:$scope.vm.focus_name,
+		    user_password:$scope.vm.focus_password
+		}).then(
+			function(data){
+				console.log(data)
+			},
+			function(err){
+				console.log(err)
+			}
 		)
-		//获取所有组织
-		apiServ.post("/api/manager/orgnization/all",{}).then(
-				function(data){
-						console.log(data)
-						$scope.aOrgnization = data;
-						$scope.orgnizationItems = Math.ceil($scope.aOrgnization.length / 5)*10;
-						
-				},
-				function(err){
-						console.log("cuowu")
-				}
+	}
+	//创建
+	$scope.create = function(){
+//		console.log($scope.vm.focus_name)
+//		console.log($scope.vm.focus_password)
+		apiServ.post("/api/manager/user/new",{
+			user_name:$scope.vm.focus_name, 
+			user_password:$scope.vm.focus_password 
+		}).then(
+			function(data){
+				console.log(data)
+			},
+			function(err){
+				console.log(err)
+			}
 		)
+	}
+	//删除
+	$scope.delete = function(){
+		apiServ.post("/api/manager/user/delete",{user_id:$scope.focus_id}).then(
+			function(data){
+				console.log(data)
+			},
+			function(err){
+				console.log(err)
+			}
+		)
+	}
+	//获取所有组织
+	apiServ.post("/api/manager/orgnization/all",{}).then(
+		function(data){
+//				console.log(data)
+			$scope.aOrgnization = data;
+			$scope.orgnizationItems = Math.ceil($scope.aOrgnization.length / 5)*10;
+			$scope.arr_orgnization = $scope.aOrgnization.slice(0,5)
+		},
+		function(err){
+				console.log("cuowu")
+		}
+	)
 	
 	
 	
