c32d78e07e259512d9dac75fcf1febf657eea2c6
gaokj <543144810@qq.com>
2016-10-19 10:57:08 +0800

    10/19 boards over

--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -6,10 +6,10 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 	  
 		apiServ.post("/api/manager/user/all",{}).then(
 			function(data){
-					for(var x in data){
-						$scope.states.push(data[x].user_name)
-					}
-					console.log($scope.states)
+				for(var x in data){
+					$scope.states.push(data[x].user_name)
+				}
+				console.log($scope.states)
 			},
 			function(err){
 					console.log("cuowu")
@@ -18,7 +18,7 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 		$scope.click = function(){
 			accountServ.login($scope.user_name,$scope.password).then(
 				function(data){
-					$state.go("user")
+					$state.go("user");
 				},function(err){
 					$scope.user_name = "";
 					$scope.password = "";
@@ -39,7 +39,7 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 	//获取所有用户
 	apiServ.post("/api/manager/user/all",{}).then(
 		function(data){
-//						console.log(data)
+			console.log(data)
 			$scope.aUser = data;
 			$scope.arr_user = $scope.aUser.slice(0,5)
 			$scope.userItems = Math.ceil($scope.aUser.length / 5)*10;
@@ -53,7 +53,6 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 		function(data){
 			console.log(data)
 			$scope.aOrgnization = data;
-			
 			$scope.orgnizationItems = Math.ceil($scope.aOrgnization.length / 5)*10;
 			$scope.arr_orgnization = $scope.aOrgnization.slice(0,5)
 		},
@@ -252,9 +251,94 @@ angular.module("cooAdmin").controller("login",function($scope,apiServ,environmen
 		)
 	}
 	//获取所有公告
-//	apiServ.post("/api/manager/board/all",{}).then(
-//		function(data){
-//			$scope.o_board=data;
-//		}
-//	)
+	apiServ.post("/api/manager/board/all",{}).then(
+//		console.log($scope.aOrgnization)//所有组
+//		console.log($scope.aUser)//所有用户
+//		console.log(data)//所有公告板
+		function(data){
+			$scope.board = data;
+			$scope.arr_board = $scope.board.slice(0,5)
+			$scope.boardItems = Math.ceil($scope.board.length / 5)*10;
+			//给data加onwer键值对
+			for(var i = 0;i < data.length;i++){
+				if(data[i].type == "personal"){
+//						alert(data[i].relative_id)
+					for(var j = 0;j < $scope.aUser.length;j++){
+						if(data[i].relative_id == $scope.aUser[j].id){
+							$scope.board[i].owner = $scope.aUser[j].user_name;
+						}
+					}
+				}else if(data[i].type == "orgnizational"){
+					for(var j = 0;j < $scope.aOrgnization.length;j++){
+						if(data[i].relative_id == $scope.aOrgnization[j].id){
+							$scope.board[i].owner = $scope.aOrgnization[j].name;
+						}
+					}
+				}
+			}
+			
+		},
+		function(err){
+			console.log(err)
+		}
+	)
+	//board分页
+	$scope.boardPage = function(){
+		$scope.arr_board = $scope.board.slice((this.currentPage-1)*5,this.currentPage*5)
+	}
+	//board焦点
+	$scope.board_focus = function(){
+		console.log(this)
+		$scope.board_id = this.x.id;
+		apiServ.post("/api/manager/card/get",{board_id:$scope.board_id}).then(
+			function(data){
+				console.log(data)
+				$scope.card = data;
+				$scope.arr_card = $scope.card.slice(0,5)
+				$scope.cardItems = Math.ceil($scope.card.length / 5)*10;
+			},
+			function(err){
+				console.log(err)
+			}
+		)
+	}
+	//card分页
+	$scope.cardPage = function(){
+		$scope.arr_card = $scope.card.slice((this.currentPage-1)*5,this.currentPage*5)
+	}
+	//card焦点
+	$scope.card_focus = function(){
+		console.log(this)
+		$scope.card_id = this.x.id;
+		apiServ.post("/api/manager/item/get",{card_id:$scope.card_id}).then(
+			function(data){
+				console.log(data)
+				var tmpData = [];
+				var _html = "";
+				for(var i = 0;i < data.length;i++){
+					tmpData = eval('('+data[i].data+')');
+//					alert(tmpData.data.url)
+					if(tmpData.type == "image"){
+						_html += '<a href="javascript:;"><img src ="'+tmpData.data.url+'" title="'+tmpData.data.title+'"/></a>'
+					}else if(tmpData.type == "text"){
+						_html += '<span>'+tmpData.data+'</span>';
+					}else if(tmpData.type == "time"){
+						_html += '<span>'+tmpData.data+'</span>'
+					}else if(tmpData.type == "checkbox"){
+						if(tmpData.data.checked == false){
+							_html += '<span><input type="checkbox" />'+tmpData.data.text+'</span>'
+						}else{
+							_html += '<span><input type="checkbox" checked/>'+tmpData.data.text+'</span>'
+						}
+					}else if(tmpData.type == "at"){
+						_html += '<span>@'+tmpData.data.name+' ID:'+tmpData.data.id+'</span>';
+					}
+				}
+				$(".boards_right").html(_html)
+			},
+			function(err){
+				console.log(err)
+			}
+		)
+	}
 })
